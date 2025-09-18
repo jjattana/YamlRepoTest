7a823004277aa4b85a5cb88ad39e5996ffca67f1
Jashanpreet Kaur Jattana <jjattana2@academic.rrc.ca>
2025-09-18 12:25:57 -0500

    updated test_main.py file

--- test_main.py
+++ test_main.py
@@ -7,7 +7,7 @@ def test_bye_returns_none():
 def test_hello_prints_expected_text(capsys):
     hello()
     captured = capsys.readouterr()
-    assert captured.out.strip() == "Hello, DevOps learner!"
+    assert captured.out.strip() == "Hello, DevOps learners!"
 def test_bye_prints_expected_text(capsys):
     bye()
     captured = capsys.readouterr()
